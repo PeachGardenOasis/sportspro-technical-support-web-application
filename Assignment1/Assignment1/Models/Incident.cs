42fa6682c90c4737ab8867cbbec076922b240685
coolatent <62825067+coolatent@users.noreply.github.com>
2021-02-20 16:47:21 -0500

    changed incidents to just incident added technicians and customers to incidentcontext file

---
+++ Assignment1/Assignment1/Models/Incident.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Incident
+    {
+        public int incidentId { get; set; }
+
+        public string incidentTitle { get; set; }
+
+        public string incidentCustomer { get; set; }
+
+        public string incidentProduct { get; set; }
+
+        public DateTime dateOpened { get; set; }
+    }
+}
