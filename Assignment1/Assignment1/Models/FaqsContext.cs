745f5cdc78fc9256e1ca590b9ec7f032d3969e47
C N <Cn512@live.com>
2021-02-20 14:53:56 -0500

    added FaqsContext class & Product class unable to create object of type "FaqsContext" due to design error on my end

---
+++ Assignment1/Assignment1/Models/FaqsContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class FaqsContext : DbContext
+    {
+        public FaqsContext(DbContextOptions<FaqsContext> options)
+            : base(options)
+        {
+
+        }
+
+        DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                    new Product { productCode = "M2MD6D", productName = "Software Package Vol. 1", productPrice = 4.99, releaseDate = DateTime.Now}
+                );
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+
+        public string productCode { get; set; }
+
+        public string productName { get; set; }
+
+        public double productPrice { get; set; }
+
+        public DateTime releaseDate { get; set; }
+    }
+}
