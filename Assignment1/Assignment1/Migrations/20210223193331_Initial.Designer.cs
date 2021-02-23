a205d118c613376cc5141b972e20def3f26d74b9
coolatent <62825067+coolatent@users.noreply.github.com>
2021-02-23 14:36:26 -0500

    old db deleted, new updated

--- Assignment1/Assignment1/Migrations/20210223193331_Initial.Designer.cs
+++ Assignment1/Assignment1/Migrations/20210223193331_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Assignment1.Migrations
 {
     [DbContext(typeof(IncidentContext))]
-    [Migration("20210221235510_Initial")]
+    [Migration("20210223193331_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
